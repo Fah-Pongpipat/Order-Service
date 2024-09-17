4f49c79090a6f800ce1e91bf21741e5c00e2968f
Fah-Pongpipat <163412313+Fah-Pongpipat@users.noreply.github.com>
2024-09-17 15:41:55 +0700

    Set up Database SQlite3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './order/entities/order.entity';
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      database: 'order.db',
+      type: 'sqlite',
+      entities: [Order],
+      synchronize: true,
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
