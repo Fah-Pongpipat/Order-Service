b4f66821dad1cfaf1cb4753f66a228708839d516
Fah-Pongpipat <163412313+Fah-Pongpipat@users.noreply.github.com>
2024-09-19 11:40:14 +0700

    Set up MessagePattern

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -21,8 +21,11 @@ export class OrderController {
   ) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
+  async create(@Body() createOrderDto: CreateOrderDto) {
+    const newOrder = await this.orderService.create(createOrderDto);
+    console.log('Order sent to Inventory for processing');
+    this.client.emit('Order_created', newOrder);
+    return newOrder;
   }
 
   @Get()
